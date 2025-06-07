653b77e37ec33c1e843415cd3767a9d14b1aaceb
Steru <Edouarrr@users.noreply.huggingface.co>
2025-06-07 22:38:29 +0000

    Update managers/__init__.py

--- managers/__init__.py
+++ managers/__init__.py
@@ -1,2 +1,19 @@
 # managers/__init__.py
-# Fichier vide pour faire du dossier un module Python
+from managers.multi_llm_manager import MultiLLMManager
+from managers.azure_blob_manager import AzureBlobManager
+from managers.azure_search_manager import AzureSearchManager
+from managers.document_manager import DocumentManager
+from managers.style_analyzer import StyleAnalyzer
+from managers.dynamic_generators import generate_dynamic_search_prompts, generate_dynamic_templates
+from managers.jurisprudence_verifier import JurisprudenceVerifier
+
+__all__ = [
+    'MultiLLMManager',
+    'AzureBlobManager', 
+    'AzureSearchManager',
+    'DocumentManager',
+    'StyleAnalyzer',
+    'generate_dynamic_search_prompts',
+    'generate_dynamic_templates',
+    'JurisprudenceVerifier'
+]
