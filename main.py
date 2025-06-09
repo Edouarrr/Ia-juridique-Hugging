98ff3812e9addfb290d7ac9da3e8be224612db92
Steru <Edouarrr@users.noreply.huggingface.co>
2025-06-09 10:22:12 +0000

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-# Test sécurisé
-if st.sidebar.checkbox("🧪 Nouvelle version"):
-    from modules import recherche_simplified
-    recherche_simplified.show_page()
-else:
-    # Votre code actuel
-    from modules import recherche
-    recherche.show_page()
